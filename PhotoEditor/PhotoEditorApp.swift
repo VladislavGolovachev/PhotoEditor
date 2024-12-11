0bdd8b8b01e238ed8ccfd1f68b25af1ccb313715
VladislavGolovachev <serpentess14@gmail.com>
2024-12-11 22:43:50 +0500

    Initial commit; also added .gitignore

---
+++ PhotoEditor/PhotoEditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoEditorApp.swift
+//  PhotoEditor
+//
+//  Created by Владислав Головачев on 11.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct PhotoEditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
